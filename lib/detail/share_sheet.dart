1302e18dd3ee01a2e48bd2b1dc6cc78999217c08
Bobby <h122656227@gmail.com>
2019-07-20 03:21:25 +0800

    finished detail reportSheet and Guess you like Widget

--- lib/detail/share_sheet.dart
+++ lib/detail/share_sheet.dart
@@ -1,42 +1,105 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/detail/report_sheet.dart';
 
 class ShareSheet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 250.0,
-      child: Row(
+      height: 300.0,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: <Widget>[
-          Expanded(
-            child: Column(
-              children: <Widget>[
-                Text('微信'),
-              ],
-           ),
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: Column(
+                  children: <Widget>[
+                    Icon(Icons.web,size: 40.0,),
+                    Text('微信'),
+                  ],
+              ),
+              ),
+              Expanded(
+                child: Column(
+                  children: <Widget>[
+                    Icon(Icons.web,size: 40.0,),
+                    Text('朋友圈'),
+                  ],
+              ),
+              ),
+              Expanded(
+                child: Column(
+                  children: <Widget>[
+                    Icon(Icons.web,size: 40.0,),
+                    Text('微博'),
+                  ],
+              ),
+              ),
+              Expanded(
+                child: Column(
+                  children: <Widget>[
+                    Icon(Icons.web,size: 40.0,),
+                    Text('QQ'),
+                  ],
+              ),
+              )
+            ],
           ),
-          Expanded(
-            child: Column(
-              children: <Widget>[
-                Text('朋友圈'),
-              ],
-           ),
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: Column(
+                  children: <Widget>[
+                    Icon(Icons.settings,size: 40.0,),
+                    Text('显示设置'),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: GestureDetector(
+                  onTap: (){
+                    Navigator.pop(context);
+                    showModalBottomSheet(
+                      context: context,
+                      builder: (BuildContext context){
+                        return ReportSheet();
+                      }
+                    );
+                  },
+                  child: Column(
+                  children: <Widget>[
+                    Icon(Icons.error_outline,size: 40.0,),
+                    Text('举报'),
+                  ],
+                ),
+                )
+              ),
+              Expanded(
+                child: Column(
+                  children: <Widget>[
+                    
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Column(
+                  children: <Widget>[
+                   
+                  ],
+                ),
+              )
+            ],
           ),
-           Expanded(
-            child: Column(
-              children: <Widget>[
-                Text('微博'),
-              ],
-           ),
+          GestureDetector(
+            onTap: (){
+              Navigator.pop(context);
+            },
+            child: Center(
+            child: Text('取消',),
           ),
-           Expanded(
-            child: Column(
-              children: <Widget>[
-                Text('QQ'),
-              ],
-           ),
           )
         ],
-      ),
+      )
     );
   }
 }
